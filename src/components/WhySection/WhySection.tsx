f237dfba78cbc299cbfcd148172825fbce4e26f2
XPoweR29 <xpower-29@wp.pl>
2023-11-06 10:29:57 +0100

    Created Why Section

--- src/components/WhySection/WhySection.tsx
+++ src/components/WhySection/WhySection.tsx
@@ -1,11 +1,42 @@
 import styles from './WhySection.module.scss';
+import { FaLeaf } from 'react-icons/fa';
+import { Icon } from '@iconify/react';
 
 export const WhySection = () => {
-    return (
-        <section className={styles.why} id="why">
-            <div className={styles.wrapper}>
-                
-            </div>
-        </section>
-    );
+	return (
+		<section className={styles.why} id='why'>
+			<div className={styles.wrapper}>
+				<h2 className={styles.why__title}>Dlaczego my?</h2>
+				<p className={styles.why__text}>
+					Z nami doświadczysz smaków, które zdobyły uznanie naszych klientów
+				</p>
+
+				<div className={styles.why__features}>
+					<div className={styles.feature}>
+						<Icon icon='fluent-emoji-high-contrast:leafy-green' className={styles.feature__icon}/>
+						<h3 className={styles.feature__name}>Świeże składniki</h3>
+						<p className={styles.feature__text}>
+							Każde dania tworzymy z najświeższych składników, aby dostarczyć Ci
+							autentyczny smak Orientu.
+						</p>
+					</div>
+                    
+					<div className={styles.feature}>
+						<Icon icon='teenyicons:discount-solid' className={styles.feature__icon}/>
+						<h3 className={styles.feature__name}>Najlepsze oferty</h3>
+						<p className={styles.feature__text}>Nasz priorytet to dostarczanie klientom najatrakcyjniejszych ofert w kuchni azjatyckiej.</p>
+					</div>
+
+					<div className={styles.feature}>
+						<Icon icon='carbon:delivery' className={styles.feature__icon}/>
+						<h3 className={styles.feature__name}>Szybka dostawa</h3>
+						<p className={styles.feature__text}>
+							Zamówienia obsługujemy błyskawicznie, aby dostarczyć Ci smaki Azji bez zbędnego czekania. Szybka dostawa to nasza specjalność.
+						</p>
+					</div>
+
+				</div>
+			</div>
+		</section>
+	);
 };
