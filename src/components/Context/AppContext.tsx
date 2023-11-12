6cd1c7dc07eb46e4eb327774e444deb83b6c60fc
XPoweR29 <xpower-29@wp.pl>
2023-11-12 08:50:49 +0100

    Reviews section has been created.

--- src/components/Context/AppContext.tsx
+++ src/components/Context/AppContext.tsx
@@ -1,8 +1,19 @@
 import { createContext } from 'react';
 
+interface Breakpoints {
+	sm: boolean;
+	md: boolean;
+	lg: boolean;
+	xl: boolean;
+	xxl: boolean;
+}
+
 type AppContextType = {
 	isMobile: boolean;
+	isLarge: boolean;
 	mediumBreakpoint: boolean;
+	breakpoint: Breakpoints;
+
 };
 
 export const AppContext = createContext<AppContextType | null>(null);
--- src/components/ReviewsSection/ReviewsSection.tsx
+++ src/components/ReviewsSection/ReviewsSection.tsx
@@ -1,50 +1,22 @@
 import styles from './ReviewsSection.module.scss';
 import lanterns from '../../assets/img/lanterns_2.png';
-import corner1 from '../../assets/img/corner_1.png';
-import corner2 from '../../assets/img/corner_2.png';
-import { AiFillStar } from 'react-icons/ai';
-import { IoIosArrowBack } from 'react-icons/io';
+import { MySwiper } from '../Swiper/MySwiper';
 
 export const ReviewsSection = () => {
-    return (
-			<section className={styles.reviews} id='reviews'>
-				<img src={lanterns} className={styles.sectionImg} />
-                
-				<div className={styles.wrapper}>
-					<h2 className={styles.reviews__title}>Co mówią nasi goście?</h2>
-					<p className={styles.reviews__text}>
-						Opinie naszych klientów są dla nas najważniejsze. Poznaj ich
-						doświadczenia i smakuj razen z nami prawdzieiw orientalne smaki
-					</p>
 
-                    <div className={styles.reviews__box}>
-                        <img src={corner1} className={styles.corner1} />
-                        <img src={corner2} className={styles.corner2} />
+	return (
+		<section className={styles.reviews} id='reviews'>
+			<img src={lanterns} className={styles.sectionImg} />
 
-                        <div className={styles.card}>
-                            <p className={styles.card__user}>Paulina Sternat</p>
-                            <div className={styles.card__rating}><AiFillStar/><AiFillStar/><AiFillStar/></div>
-                            <p className={styles.card__text}>I liket it all very much. Świetne jedzenie i jeszcze lepsza obsługa. Polecam gorąco jeśli tylko będziecie w pobliżu!</p>
-                        </div>
+			<div className={styles.wrapper}>
+				<h2 className={styles.reviews__title}>Co mówią nasi goście?</h2>
+				<p className={styles.reviews__text}>
+					Opinie naszych klientów są dla nas najważniejsze. Poznaj ich
+					doświadczenia i smakuj razen z nami prawdzieiw orientalne smaki
+				</p>
 
-                        <div className={styles.card}>
-                            <p className={styles.card__user}>Paulina Sternat</p>
-                            <div className={styles.card__rating}><AiFillStar/><AiFillStar/><AiFillStar/></div>
-                            <p className={styles.card__text}>I liket it all very much. Świetne jedzenie i jeszcze lepsza obsługa. Polecam gorąco jeśli tylko będziecie w pobliżu!</p>
-                        </div>
-
-                        <div className={styles.card}>
-                            <p className={styles.card__user}>Paulina Sternat</p>
-                            <div className={styles.card__rating}><AiFillStar/><AiFillStar/><AiFillStar/></div>
-                            <p className={styles.card__text}>I liket it all very much. Świetne jedzenie i jeszcze lepsza obsługa. Polecam gorąco jeśli tylko będziecie w pobliżu!</p>
-                        </div>
-                    </div>
-
-                        <div className={styles.reviews__buttons}>
-                            <button className={styles.prev}><IoIosArrowBack/></button>
-                            <button className={styles.next}><IoIosArrowBack/></button>
-                        </div>
-				</div>
-			</section>
-		);
-}
+				<MySwiper />
+			</div>
+		</section>
+	);
+};
